65801453b749754afa13c075181a765200fb2846
Si Dao <si.dao@outlook.com>
2022-12-30 04:26:20 +0000

    Finished bootable Linux on QEMU

--- src/pack-rootfs.sh
+++ src/pack-rootfs.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 ARTIFACT_DIR=${1:-$PWD}
-OUTPUT_DIR=${2:-$PWD}
+OUTPUT_DIR=${2:-${ARTIFACT_DIR}}
 
-cat > boot.cmd <<-EOF
-    fatload virtio 0:1 0x45000000 /Image
-
-    setenv bootargs "root=/dev/ram0 rdinit=/sbin/init"
-    booti 0x45000000 - 0x40000000
-EOF
 # generate boot script (u-boot boot script format)
 ${ARTIFACT_DIR}/mkimage -A arm -T script -C none -n "Xen QEMU boot script" -d boot.cmd ${ARTIFACT_DIR}/boot.scr
 ${ARTIFACT_DIR}/mkimage -A arm -T ramdisk -C gzip -d ${ARTIFACT_DIR}/initrd.img ${ARTIFACT_DIR}/initrd.img.gz
+
+cat > boot.cmd <<-EOF
+    fatload virtio 0:1 0x48000000 /Image
+    fatload virtio 0:1 0x48800000 /initrd.img.gz
+
+    setenv bootargs "root=/dev/ram0 rdinit=/sbin/init"
+    booti 0x48000000 0x48800000 0x40000000
+EOF
 
 cat > genimage.cfg <<- EOF
     image boot.vfat {
